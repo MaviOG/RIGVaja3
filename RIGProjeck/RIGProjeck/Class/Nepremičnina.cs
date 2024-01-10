aa8df32056b1bdc2e3acc7c79ea4114e2e704f2b
AljazRant <aljaz.rant@gmail.com>
2024-01-10 11:06:46 +0100

    dodanje funkcije za :Prijava
    Pregled zgodovine objav uporabnika
    
    Zadnje objave
    
    Povečanje vidljivosti oglasa z doplačilom
    
    Verifikacija zaupanim uporabnikom
    
    Registracija

--- RIGProjeck/RIGProjeck/Class/Nepremičnina.cs
+++ RIGProjeck/RIGProjeck/Class/Nepremičnina.cs
@@ -2,20 +2,24 @@
 {
     public class Nepremičnina
     {
-        public int NepremičninaID {  get; set; }
-        public string lokacija {  get; set; }
-        public int cena {  get; set; }
-        public DateOnly dan_izgradnje {  get; set; }
-        public int stevilo_kopalnic {  get; set; }
-        public int stevilo_prostorov {  get; set; }
-        public int velikost {  get; set; }
-        public int stevilo_balkonov {  get; set; }
+
+        public int NepremičninaID { get; set; }
+        public string lokacija { get; set; }
+        public int cena { get; set; }
+        public DateOnly dan_izgradnje { get; set; }
+        public int stevilo_kopalnic { get; set; }
+        public int stevilo_prostorov { get; set; }
+        public int velikost { get; set; }
+        public int stevilo_balkonov { get; set; }
         public int stevilo_spalnic { get; set; }
-        public List <int> Ocena { get; set; }
+        public int uporabnikov_id {  get; set; }
+        public List<int> Ocena { get; set; }
         public List<string> Komentarji { get; set; }
+        public List<Nepremičnina> BoostedAds {get;set;}
+		public List<Nepremičnina> UporabnikoveObjave { get; set; }
+        public List<Nepremičnina> VseObjave { get; set; }
 
-
-        public Nepremičnina(string lokacija, int cena, DateOnly dan_izgradnje, int stevilo_kopalnic,int velikost,int stevilo_prostorov,int stevilo_balkonov,int stevilo_spalnic)
+		public Nepremičnina(string lokacija, int cena, DateOnly dan_izgradnje, int stevilo_kopalnic,int velikost,int stevilo_prostorov,int stevilo_balkonov,int stevilo_spalnic, int uporabnikov_id)
         {
             this.lokacija = lokacija;
             this.cena = cena;
@@ -25,7 +29,31 @@
             this.stevilo_prostorov = stevilo_prostorov;
             this.stevilo_balkonov = stevilo_balkonov;
             this.stevilo_spalnic = stevilo_spalnic;
+            this.uporabnikov_id = uporabnikov_id;
         }
-        
-    }
+		public void BoostProperty(Nepremičnina adToBoost)
+		{
+			BoostedAds.Add(adToBoost);
+		}
+		public void RemoveBoost(Nepremičnina adToRemoveBoost)
+		{
+			BoostedAds.Remove(adToRemoveBoost);
+		}
+		public void LoadRecents()
+		{
+			//nalozi se VseObjave pa omeji na določeno stevilo oz. se ne omeji koliko se jih prikaže
+		}
+
+		public void LoadUserAds(int uporabnikovId)
+		{
+            foreach (var item in VseObjave)
+            {
+                if (item.uporabnikov_id == uporabnikovId )
+                {
+                    UporabnikoveObjave.Add(item);
+                }
+                
+            }
+		}
+	}
 }
---
+++ RIGProjeck/RIGProjeck/Class/User.cs
@@ -0,0 +1,67 @@
+﻿namespace RIGProjeck.Class
+{
+	public class User
+	{
+		private int userid { get; set; }
+		private string username { get; set; }
+		private string password { get; set; }
+		private string email { get; set; }
+		private bool verified { get; set; }
+
+		private List<User> users { get; set; }
+
+		public List<Nepremičnina> uporabnikoveObjave { get; set; }
+
+		public User(int userid,string username, string password, string email, bool verified)
+		{
+			this.userid = userid;
+			this.username = username;
+			this.password = password;
+			this.email = email;
+			this.verified = verified;
+		}
+		public bool Register(string Username, string Password, string Email)
+		{
+			int id = users.Count+1;
+			password = Password;
+			username = Username;
+			email = Email;
+			foreach (var user in users)
+			{
+				if (username == user.username || email == user.email)
+				{
+					return false;
+				}
+				users.Add(new User(id,username, password, email, false));
+
+			}
+			return true;
+		}
+
+		public bool LogIn(string password, string username)
+		{
+			foreach (var user in users)
+			{
+				if (user.username == username && user.password == password) 
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		public void LogOut()
+		{
+			//iz vmesnika se prestavi na login/register stran
+			throw new System.NotImplementedException("Not implemented");
+		}
+
+		public void VerifyUser(User selectedUser)
+		{
+			if (users.Count != 0 && users != null)
+			{
+				users.Find(p => p.userid == selectedUser.userid).verified = true;
+			}
+
+		}
+	}
+}
